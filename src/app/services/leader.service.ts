40855d0099c8050a04ee4ad484393dc6482fcb92
Chinmay Gulhane <chinmay.gulhane@gmail.com>
2020-11-11 11:16:48 +0530

    RxJs Part 1

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,6 +1,9 @@
 import { LEADERS } from './../shared/leaders';
 import { Leader } from './../shared/leader';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,24 +12,16 @@ export class LeaderService {
 
   constructor() { }
 
-  getLeaders(): Promise<Leader[]> {
-    return new Promise(resolve => {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(LEADERS), 2000);
-    });
+  getLeaders(): Observable<Leader[]> {
+    return of(LEADERS).pipe(delay(2000));
+
   }
 
-  getLeader(id: string): Promise<Leader> {
-    return new Promise(resolve => {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(LEADERS.filter((promo) => (promo.id === id))[0]), 2000);
-    });
+  getLeader(id: string): Observable<Leader> {
+    return of(LEADERS.filter((leader) => (leader.id === id))[0]).pipe(delay(2000));
   }
 
-  getFeaturedLeader(): Promise<Leader> {
-    return  new Promise(resolve => {
-      // Simulate server latency with 2 second delay
-        setTimeout(() => resolve(LEADERS.filter((promo) => promo.featured)[0]), 2000);
-    });
+  getFeaturedLeader(): Observable<Leader> {
+    return of(LEADERS.filter((leader) => leader.featured)[0]).pipe(delay(2000));
   }
 }
